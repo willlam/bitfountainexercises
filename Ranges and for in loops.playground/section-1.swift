562fb9e1ae84296cad7327ad2b0e5eac873da1e1
Will Lam <will@willlam.net>
2015-02-19 12:50:04 -0500

    learning for-in and range loops in Swift

---
+++ Ranges and for in loops.playground/section-1.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var cafeNames = ["Dineen Coffee", "Te Aro Coffee", "Crema Coffee", "Lit Espresso Bar", "Mercury Espresso Bar"]
+
+for cafeName in cafeNames {
+  println(cafeName)
+}
+
+// in a For-In Loop what happens here is for the variable cafeName, it goes into array cafeNames and prints out each instance in the array within the curly braces and assigns it to cafeName
+
+for x in 1...3 {
+  println(x)
+}
+
+// for a range loop, within the range that you declare (in this case 1 to 3), the loop will iterate over teh range that you declare and assign it to the variable x and print each instance of that range
