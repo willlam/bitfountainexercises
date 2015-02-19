a5aaf134ee7a32584eb835e780b991c39ae775a7
Will Lam <will@willlam.net>
2015-02-19 13:04:47 -0500

    added for in enumerate loop

--- Ranges and for in loops.playground/section-1.swift
+++ Ranges and for in loops.playground/section-1.swift
@@ -13,3 +13,9 @@ for x in 1...3 {
 }
 
 // for a range loop, within the range that you declare (in this case 1 to 3), the loop will iterate over teh range that you declare and assign it to the variable x and print each instance of that range
+
+for (index, cafeName) in enumerate(cafeNames) {
+  println("index: \(index) cafeName: \(cafeName)")
+}
+
+// for in enumerate loop access both the index of the array and prints out the elements in the cafeNames array
