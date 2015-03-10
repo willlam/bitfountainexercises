b99dc3c662d0947d7335dcd2914a754902766ff5
Will Lam <will@willlam.net>
2015-03-10 09:13:16 -0400

    switch statements. Hoo Rah.

---
+++ SwitchStatements.playground/section-1.swift
@@ -0,0 +1,18 @@
+// Switch Statements
+
+import UIKit
+
+var x = 4
+
+switch x {
+case 1:
+  println("The value of x is 1")
+case 2:
+  println("The value of x is 2")
+case 3:
+  println("The value of x is 3")
+case 4:
+  println("The value of x is 4")
+default:
+  println("default value printed")
+}
