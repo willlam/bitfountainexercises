f5177bd56751acbc42fd01302b389546839d98c5
Will Lam <will@willlam.net>
2015-03-17 22:51:55 -0400

    started 99 Balloons project - randomizing image and facts using structs and functions

---
+++ 99Balloons/99Balloons/Balloon.swift
@@ -0,0 +1,11 @@
+//
+//  Balloon.swift
+//  99Balloons
+//
+//  Created by William Lam on 2015-03-10.
+//  Copyright (c) 2015 William Lam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
---
+++ 99Balloons/99Balloons/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  99Balloons
+//
+//  Created by William Lam on 2015-03-10.
+//  Copyright (c) 2015 William Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var balloonsLabel: UILabel!
+  @IBOutlet weak var backgroundImageView: UIImageView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func nextBalloonBarButtonItemPressed(sender: UIBarButtonItem) {
+  }
+
+}
+
