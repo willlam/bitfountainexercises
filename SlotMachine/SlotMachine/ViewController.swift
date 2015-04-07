0f9d795d4076928eb171118a2fd0c042ca7e0efd
Will Lam <will@willlam.net>
2015-04-06 23:44:59 -0400

    added third container to views in code  and adding some labels

--- SlotMachine/SlotMachine/ViewController.swift
+++ SlotMachine/SlotMachine/ViewController.swift
@@ -17,15 +17,36 @@ class ViewController: UIViewController {
   var thirdContainer: UIView!
   var fourthContainer: UIView!
   
+  var titleLabel: UILabel!
+  
+  // Information Labels
   
   // the k in kMarginForView denoates that it's a constant
+  var creditsLabel: UILabel!
+  var betLabel: UILabel!
+  var winnerPaidLabel: UILabel!
+  var creditsTitleLabel: UILabel!
+  var betTitleLabel: UILabel!
+  var winnerPaidTitleLabel: UILabel!
+  
   let kMarginForView: CGFloat = 10.0
+  let kMarginForSlot: CGFloat = 2.0
+  
   let kSixth: CGFloat = 1.0/6.0
+  let kThird: CGFloat = 1.0/3.0
 
+
+  let kNumberOfContainers = 3
+  let kNumberOfSlots = 3
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    self.setUpContainerViews()
+    self.setupContainerViews()
+    
+    // need to let setup the container views first before working with individual containers and drawing into them.
+    setupFirstContainer(self.firstContainer)
+    setupSecondContainer(self.secondContainer)
   }
 
   override func didReceiveMemoryWarning() {
@@ -35,11 +56,86 @@ class ViewController: UIViewController {
 
   //helper function to abstract away some logic from ViewDidLoad
   
-  func setUpContainerViews (){
+  func setupContainerViews (){
     self.firstContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: self.view.bounds.origin.y, width: self.view.bounds.width - (kMarginForView*2), height: self.view.bounds.height*kSixth))
     self.firstContainer.backgroundColor = UIColor.redColor()
     self.view.addSubview(self.firstContainer)
+    
+    self.secondContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: firstContainer.frame.height, width: self.view.bounds.width - (kMarginForView*2), height: self.view.bounds.height*(3*kSixth)))
+    self.secondContainer.backgroundColor = UIColor.blackColor()
+    self.view.addSubview(self.secondContainer)
+    
+    self.thirdContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: firstContainer.frame.height + secondContainer.frame.height, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
+    self.thirdContainer.backgroundColor = UIColor.greenColor()
+    self.view.addSubview(self.thirdContainer)
+    
+    self.fourthContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: firstContainer.frame.height + secondContainer.frame.height + thirdContainer.frame.height, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
+    self.fourthContainer.backgroundColor = UIColor.blueColor()
+    self.view.addSubview(self.fourthContainer)
   }
-
+  
+  func setupFirstContainer(containerView: UIView) {
+    self.titleLabel = UILabel()
+    self.titleLabel.text = "Super Slots"
+    self.titleLabel.textColor = UIColor.yellowColor()
+    self.titleLabel.font = UIFont(name: "MarkerFelt-Wide", size: 40)
+    self.titleLabel.sizeToFit()
+    self.titleLabel.center = containerView.center
+    containerView.addSubview(self.titleLabel)
+  }
+  
+  func setupSecondContainer(containerView: UIView) {
+    for var containerNumber = 0; containerNumber < kNumberOfContainers; ++containerNumber {
+      for var slotNumber = 0; slotNumber < kNumberOfSlots; ++slotNumber {
+        var slotImageView = UIImageView()
+        slotImageView.backgroundColor = UIColor.yellowColor()
+        slotImageView.frame = CGRect(x: containerView.bounds.origin.x + (containerView.bounds.size.width * CGFloat(containerNumber) * kThird), y: containerView.bounds.origin.y + (containerView.bounds.size.height * CGFloat(slotNumber) * kThird), width: containerView.bounds.width * kThird - kMarginForSlot, height: containerView.bounds.height * kThird - kMarginForSlot)
+        containerView.addSubview(slotImageView)
+      }
+    }
+    
+  }
+  
+  func setupThirdContainer(containerView: UIView) {
+    
+    self.creditsLabel = UILabel()
+    self.creditsLabel.text = "000000"
+    self.creditsLabel.textColor = UIColor.redColor()
+    self.creditsLabel.font = UIFont(name: "Menlo-Bold", size: 16)
+    self.creditsLabel.sizeToFit()
+    
+    // the center code isn't re-used because we don't want our labels to be stacked on top of one another
+    self.creditsLabel.center = CGPoint(x: containerView.frame.width * kSixth, y: containerView.frame.height * kThird)
+    self.creditsLabel.textAlignment = NSTextAlignment.Center
+    self.creditsLabel.backgroundColor = UIColor.darkGrayColor()
+    containerView.addSubview(self.creditsLabel)
+    
+    self.betLabel = UILabel()
+    self.betLabel.text = "0000"
+    self.betLabel.textColor = UIColor.redColor()
+    self.betLabel.font = UIFont(name: "Menlo-Bold", size: 16)
+    self.betLabel.sizeToFit()
+    // moved over to the right
+    self.betLabel.center = CGPoint(x: containerView.frame.width * kSixth * 3, y: containerView.frame.height * kThird)
+    self.betLabel.textAlignment = NSTextAlignment.Center
+    self.betLabel.backgroundColor = UIColor.darkGrayColor()
+    containerView.addSubview(self.betLabel)
+    
+    self.winnerPaidLabel = UILabel()
+    self.winnerPaidLabel.text = "000000"
+    self.winnerPaidLabel.textColor = UIColor.redColor()
+    self.winnerPaidLabel.font = UIFont(name: "Menlo-Bold", size: 16)
+    self.winnerPaidLabel.sizeToFit()
+    
+    // moving the winnerPaidLabel more to the right
+    self.winnerPaidLabel.center = CGPoint(x: containerView.frame.width * kSixth * 5, y: containerView.frame.height * kThird)
+    self.winnerPaidLabel.textAlignment = NSTextAlignment.Center
+    self.winnerPaidLabel.backgroundColor = UIColor.darkGrayColor()
+    containerView.addSubview(self.winnerPaidLabel)
+    
+    
+    
+  }
+  
 }
 
