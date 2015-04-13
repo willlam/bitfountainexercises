2f5cc44b9ca270b487fc827f0f822d949e4424da
Will Lam <will@willlam.net>
2015-04-13 19:47:42 -0400

    created slot struct and added image assets

---
+++ SlotMachine/SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by William Lam on 2015-04-13.
+//  Copyright (c) 2015 William Lam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+  var value = 0
+  var image = UIImage(named: "Ace")
+  var isRed = true
+}
